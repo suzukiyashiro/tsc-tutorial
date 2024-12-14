b6504a36e478eb0b2bf89b015ee57fd2c0fe1cf1
suzukiyashiro <j2100095@gunma-u.ac.jp>
2024-12-15 08:08:56 +0900

    1.基本型

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("hello typescript")
+// 1.基本型
+let email: string = "user@gmail.com";
+let age: number = 25;
+let isStudent: boolean = true;
+
+console.log(email, age, isStudent);
+
+let email2 = "user@gmail.com"; // 型推論
+
+email = "user1@gmail.com";
+// email = true;
+console.log(email);
+
