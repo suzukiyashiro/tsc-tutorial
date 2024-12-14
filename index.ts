2de498b446d1c497c94a9a50d19ae351ee3f850b
suzukiyashiro <j2100095@gunma-u.ac.jp>
2024-12-15 08:09:46 +0900

    2.配列

--- index.ts
+++ index.ts
@@ -3,11 +3,14 @@ let email: string = "user@gmail.com";
 let age: number = 25;
 let isStudent: boolean = true;
 
-console.log(email, age, isStudent);
+// console.log(email, age, isStudent);
 
 let email2 = "user@gmail.com"; // 型推論
 
 email = "user1@gmail.com";
 // email = true;
-console.log(email);
+// console.log(email);
 
+// 2.配列
+let numbers: number[] = [1, 2, 3, 4];
+console.log(numbers)
